1f90a2026b0155d8743f26ae6bc99fbda69732a6
Stephen Blair <stephen@stephenjohnblair.com>
2021-01-27 14:12:38 +0000

    Updated README file

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,12 +7,34 @@ export function PageHeader() {
   return (
     <Flex justifyContent="space-between" alignItems="center" pt="15px">
       <Brand />
-      <Stack isInline>
-        <Button leftIcon={<FaGithub />}>View Source</Button>
-        <Button leftIcon={<FaChrome />} colorScheme="red">
+      <Stack isInline d={["none", "none", "flex", "flex"]}>
+        <Button
+          as="a"
+          href="https://github.com/stephenjblair/githunt"
+          target="_blank"
+          leftIcon={<FaGithub />}
+          bg="gray.700"
+          color="white"
+          _hover={{ bg: "gray.900" }}
+        >
+          View Source
+        </Button>
+        <Button
+          as="a"
+          href="https://chrome.google.com/webstore/detail/githunt/khpcnaokfebphakjgdgpinmglconplhp"
+          target="_blank"
+          leftIcon={<FaChrome />}
+          colorScheme="red"
+        >
           Use Extension
         </Button>
-        <Button leftIcon={<FaTwitter />} colorScheme="purple">
+        <Button
+          as="a"
+          href="https://twitter.com/intent/tweet?text=GitHunt%20%E2%80%93%20Most%20starred%20projects%20on%20Github%20by%20@stephenjohnbla4%20https://github.com/stephenjblair/githunt"
+          leftIcon={<FaTwitter />}
+          background="#1DA1F2"
+          color="#fff"
+        >
           Tweet
         </Button>
       </Stack>
